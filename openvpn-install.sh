7c4cc717a7734c4605fdba9233743270deebbddf
mercik <mercik85@mail.ru>
2024-01-27 06:27:26 +0500

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -218,6 +218,7 @@ access-control: fd42:42:42:42::/112 allow' >>/etc/unbound/openvpn.conf
 
 function installQuestions() {
 	echo "Welcome to the OpenVPN installer!"
+    echo "The git repository is available at: https://github.com/mercik/openvpn-install"
 	echo "The git repository is available at: https://github.com/angristan/openvpn-install"
 	echo ""
 
